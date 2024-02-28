14a0907d6643169917f5f8e43b5f1692d0530fe5
Alexis Murray <alexjacobmurray@gmail.com>
2024-02-28 09:02:04 -0800

    First commit

--- linkedlist.c
+++ linkedlist.c
@@ -8,6 +8,14 @@ struct node {
 // 02/28 exit ticket
 void printLinkedList(node* head) {
     //TODO: print the values in the linked list, e.g. 0->1->2
+    node* current = head;
+    while (current != NULL) {
+        printf("%d", current->value);
+        if (current->next != NULL) {
+            printf("->");
+        }
+        current = current->next;
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -18,6 +26,14 @@ int main(int argc, char* argv[]) {
     head->next = NULL;    
 
     //TODO: add a node with value 2 at the end of the linked list
+    head->next = malloc(sizeof(node));
+    head->next->value = 2;
+    head->next->next = NULL;
 
     //TODO: add a node with value 0 at the beginning of the linked list 
+    struct node* temp = malloc(sizeof(node));
+    temp->value = 0;
+    temp->next = head;
+    head = temp;
+    free(temp);
 } 
